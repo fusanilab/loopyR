92c7d8f5379a6f526fa81994bac706b8c7c5b8a5
dancmann <danmann23@gmail.com>
2021-03-18 18:52:38 +0100

    added function that makes request to Loopy API

---
+++ R/loopy_api.R
@@ -0,0 +1,111 @@
+#' Call Loopy API
+#'
+#' @description Wrapper for httr::GET that handles Loopy API requests.
+#' @param url string Loopy URL file path (origin should be set with set_loopy_user)
+#' @param verbose string
+#' @param video_id
+#' @param collection_id
+#'
+#' @return
+
+
+loopy_api <- function(url,
+                      verbose = "0",
+                      video_id = NULL,
+                      collection_id = NULL) {
+
+  if (!as.character(verbose) %in% c("0", "1")) {
+    warning("verbose= must be either '0' or '1'; defaulting to '0' (only user's files)")
+    verbose <- "0"
+  } else {
+    # convert to character just to be safe
+    verbose <- as.character(verbose)
+  }
+
+  # Creates a list and then filters out those that are set to NULL.
+    # Some of the values in Loopy ids are "NULL" so there has to be a way to distinguish between those where the id=NULL and when the user doesn't know or care what the id is.
+  params <- list(
+    "video_id" = video_id,
+    "collection_id" = collection_id,
+    "verbose" = verbose
+  )
+
+  # Removes NULLs so that values won't be included in the parameters
+  params[sapply(params, is.null)] <- NULL
+
+  # Get the values defined with set_loopy_user
+  #? If a user wants to create a local .Renviron file will this still find it?
+  loopy_origin <- Sys.getenv("LOOPY_ORIGIN")
+  api <- Sys.getenv("LOOPY_API_KEY")
+
+  # Make sure that the user properly set loopy origin and api key
+  if (nchar(loopy_origin) == 0 | nchar(api) == 0) {
+    stop(
+      "Loopy URL and/or Loopy API key not set.\n",
+      "Check that .Renviron file exists.\n",
+      "You can set Loopy URL and API key by using set_loopy_user function"
+    )
+  }
+
+  response <- httr::GET(
+    url = sprintf("%s%s", loopy_origin, url),
+    httr::add_headers(
+      "Content-Type" = "application/json",
+      "X-Api-Key" = api
+    ),
+    query = params
+  )
+
+  # Check for json or xml.
+  # The xml check is probably not necessary.
+  # Most of the relevant info is json format
+  if (httr::http_type(response) == "application/json") {
+    parsed <-
+      jsonlite::fromJSON(httr::content(
+        response,
+        "text",
+        encoding = "UTF-8"
+      ),
+      simplifyVector = FALSE
+      )
+  } else if (httr::http_type(response) == "text/html") {
+    parsed <-
+      xml2::read_html(httr::content(response, "text"))
+    message("API returned html, not json")
+  } else {
+    stop("API did not return json or html", call. = FALSE)
+  }
+
+
+  if (httr::http_error(response)) {
+    stop(
+      sprintf(
+        "Loopy API request failed [%s]\n%s\n<%s>",
+        httr::status_code(response),
+        parsed$message,
+        parsed$documentation_url
+      ),
+      call. = FALSE
+    )
+  }
+
+
+  # Organizes the httr response
+  # Eventually this should be made into a cleaner response
+  structure(
+    list(
+      content = parsed,
+      path = response$url,
+      response = response$status_code
+    ),
+    class = "loopy_api"
+  )
+}
+
+print.loopy_api <- function(x, ...) {
+  # Defines print structure for loopy_api objects
+  cat("<Loopy ", x$path, ">\n", sep = "")
+  cat("Status code: ", x$response, "\n")
+  str(x$content)
+  invisible(x)
+}
