2831b27461a446a2bfa3dafc6dde4bb2b7b76690
dancmann <danmann23@gmail.com>
2021-03-24 14:38:17 +0100

    formatting and documentation

--- R/download_loopy_results.R
+++ R/download_loopy_results.R
@@ -1,71 +1,66 @@
 
-#' Download Results Data
+#' Download Tracking Data
 #'
-#' @description
+#' @description Downloads and returns tracking data as a data frame.
 #'
-#' @param
-#' @param
-#' @param
+#' @param asset_id string The asset_id that Loopy creates for each result.
+#' @param group_key string The group_key that Loopy creates for each result.
 #'
-#' @return
-#'
-
+#' @return data.frame Tracking data.
 
 download_tracking_data <-
-    function(asset_id, group_key) {
-        # This function uses the main loopy_api function, but the call might be different enough to warrant a whole new function.
-        url <- sprintf("/api/1.0/result/download/asset/%s", asset_id)
+  function(asset_id, group_key) {
+    # This function uses the main loopy_api function, but the call might be different enough to warrant a whole new function.
+    url <- sprintf("/api/1.0/result/download/asset/%s", asset_id)
 
-        response <- loopy_api(
-            url = url,
-            asset_id = asset_id,
-            group_key = group_key,
-            format = "csv",
-            verbose = NULL
-        )
+    response <- loopy_api(
+      url = url,
+      asset_id = asset_id,
+      group_key = group_key,
+      format = "csv",
+      verbose = NULL
+    )
 
-
-
-        if(response$status_code == 202){
-            # Need to add in a check and wait.
-            try <- 0
-            time <- 1
-            n_tries <- 5
-            while(try < n_tries){
-                job_status <- check_job_status(response$content$job_id)
-                if(job_status == 200){
-                    break
-                }else{
-                    Sys.sleep(time = time)
-                    time <- time*2
-                    try <- try + 1
-                }
-            }
-
-            if(try == 5){
-
-                warning("Request timed out: Try call again later")
-
-            } else {
-                # Make the call again
-                response <- loopy_api(
-                    url = url,
-                    asset_id = asset_id,
-                    group_key = group_key,
-                    format = "csv"
-                )
-            }
+    if (response$status_code == 202) {
+      # Checks the job status of the request. If status turns to 200 it downloads the data.
+      try <- 0
+      time <- 1
+      # Makes a maximum of 5 attempts
+      n_tries <- 5
+      while (try < n_tries) {
+        job_status <- check_job_status(response$content$job_id)
+        if (job_status == 200) {
+          break
+        } else {
+          Sys.sleep(time = time)
+          # exponential backoff
+          time <- time * 2
+          try <- try + 1
         }
+      }
 
-
-        return(response$content)
+      if (try == 5) {
+        warning("Request timed out: Try call again later")
+      } else {
+        # Make the call again
+        response <- loopy_api(
+          url = url,
+          asset_id = asset_id,
+          group_key = group_key,
+          format = "csv"
+        )
+      }
     }
+
+    return(response$content)
+  }
 
 
 
 #' Check job status
 #'
-#' @description
+#' @description Checks if a Loopy API request is in progress, finished, or failed.
+#' Function needs quite a bit of checking.
 #'
 #' @param
 #'
@@ -73,23 +68,22 @@ download_tracking_data <-
 #'
 #'
 check_job_status <-
-    function(job_id){
-        #
-        url <- sprintf("/api/1.0/job/%s/status", job_id)
+  function(job_id) {
+    # This function needs t
+    url <- sprintf("/api/1.0/job/%s/status", job_id)
 
-        response <- loopy_api(url, verbose = '1')
+    response <- loopy_api(url, verbose = "1")
 
-        if(response$content$status == "finished"){
-            message("Job is finished: Data has been prepared/cached")
-            out <- response$status_code
-        }else if(response$status_code == 202){
-            message("Data is being prepared")
-            out <- response$status_code
-        }else{
-            ## I have to do more testing to see what the range of responses are.
-            warning("Data preparation may have failed.")
-            out <- response$status_code
-        }
-        return(out)
+    if (response$content$status == "finished") {
+      message("Job is finished: Data has been prepared/cached")
+      out <- response$status_code
+    } else if (response$status_code == 202) {
+      message("Data is being prepared")
+      out <- response$status_code
+    } else {
+      ## I have to do more testing to see what the range of responses are.
+      warning("Data preparation may have failed.")
+      out <- response$status_code
     }
-
+    return(out)
+  }
