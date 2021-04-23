008ed86ca7e8aacd4dea6f1dd0890215ec31097a
Dan C. Mann <danmann23@gmail.com>
2021-04-23 20:00:39 +0200

    added check to ensure the changes are made to the file

--- tests/test_loopy_user.R
+++ tests/test_loopy_user.R
@@ -10,12 +10,20 @@ testthat::expect_message(
 )
 
 # If a .Renviron file already exists then the user should get a different message.
+api_2 <- '53c0nd3xamp134P1k3y'
+origin_2 <- 'http://second-example-vpn:0000'
+
 testthat::expect_message(
-  set_loopy_user(api_key = '53c0nd3xamp134P1k3y',
-                 loopy_origin = 'http://second-example-vpn:0000',
+  set_loopy_user(api_key = api_2,
+                 loopy_origin = origin_2,
                  renviron_path = "./tests/"),
   "API key and Loopy URL origin written to .Renviron.\nPlease restart your R session to use the Loopy API key and URL."
 )
+
+# Check that changes are made
+renv <- unlist(strsplit(readLines('./tests/.Renviron'), '='))
+testthat::expect_match(renv[2], api_2)
+testthat::expect_match(renv[4], origin_2)
 
 # Remove the useless .Renviron file.
 file.remove("./tests/.Renviron")
