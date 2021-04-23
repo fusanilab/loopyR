5a2411548660e57a7439ad7aa92b2393731abbf0
Dan C. Mann <danmann23@gmail.com>
2021-04-23 19:16:49 +0200

    removed unnecessary comma in first test, and added comments for removing test file

--- tests/test_loopy_user.R
+++ tests/test_loopy_user.R
@@ -6,8 +6,7 @@ testthat::expect_message(
   set_loopy_user(api_key = '3xamp134P1k3y',
                  loopy_origin = 'http://example-vpn:0000',
                  renviron_path = "./tests/"),
-  ".Renviron file created at ./tests/",
-
+  ".Renviron file created at ./tests/"
 )
 
 # If a .Renviron file already exists then the user should get a different message.
@@ -18,7 +17,7 @@ testthat::expect_message(
   "API key and Loopy URL origin written to .Renviron.\nPlease restart your R session to use the Loopy API key and URL."
 )
 
-
+# Remove the useless .Renviron file.
 file.remove("./tests/.Renviron")
 
 
